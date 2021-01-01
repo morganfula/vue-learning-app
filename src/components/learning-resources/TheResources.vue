34abff654598e0f9627d98b9d42090a4fa46ed0b
morganfula <morgan.fula@me.com>
2021-01-01 09:47:47 +0100

    tab for resouces done

--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -1,9 +1,13 @@
 <template>
   <base-card>
-    <base-button @click="setSelectedTab('stored-resources')"
+    <base-button
+      @click="setSelectedTab('stored-resources')"
+      :mode="storedResButtonMode"
       >Stored Resources</base-button
     >
-    <base-button @click="setSelectedTab('add-resources')"
+    <base-button
+      @click="setSelectedTab('add-resources')"
+      :mode="addResButtonMode"
       >Add Resource
     </base-button>
   </base-card>
@@ -22,6 +26,33 @@ export default {
   data() {
     return {
       selectedTab: 'stored-resources',
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue-js documentation.',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to search anything',
+          link: 'https://google.org',
+        },
+      ],
+    };
+  },
+  computed: {
+    storedResButtonMode() {
+      return this.selectedTab === 'stored-resources' ? null : 'flat';
+    },
+    addResButtonMode() {
+      return this.selectedTab === 'add-resources' ? null : 'flat';
+    },
+  },
+  provide() {
+    return {
+      resources: this.storedResources,
     };
   },
   methods: {
